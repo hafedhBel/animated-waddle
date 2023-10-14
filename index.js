74eab4d5dc1d775a0c722c68976c328f672be326
Hafedh Ben Haj Ali <70435711+hafedhBel@users.noreply.github.com>
2023-10-14 23:52:22 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
+    res.send("<h1>ahla b daglouch</h1>");
 });
 
 app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+    res.send("<h1>97772722</h1>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>Hafedh Ben Haj Ali </h1>");
 });
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+    console.log(`Hello World  my server has started on port ${port}`);
 });
